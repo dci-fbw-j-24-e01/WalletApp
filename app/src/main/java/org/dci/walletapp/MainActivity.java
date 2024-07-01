55c7ca3cce28afc60b61e27bbbd32dd1fac25099
indaleci <indaleci4@gmail.com>
2024-07-01 15:02:26 +0200

    "Added TextBoxes and Buttons to the main activity"

--- app/src/main/java/org/dci/walletapp/MainActivity.java
+++ app/src/main/java/org/dci/walletapp/MainActivity.java
@@ -2,6 +2,7 @@ package org.dci.walletapp;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +12,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView welcomeText;
+    TextView currentBalance;
     Button addIncomeButton;
     Button addExpenseButton;
     Button historyButton;
@@ -18,31 +21,21 @@ public class MainActivity extends AppCompatActivity {
     Button categoryManagementButton;
     Button supportButton;
 
-
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-
-        addIncomeButton.findViewById(R.id.addIncomeButton);
-        addExpenseButton.findViewById(R.id.addExpenseButton);
-        profileButton.findViewById(R.id.profileButton);
-        categoryManagementButton.findViewById(R.id.categoryManagementButton);
-        supportButton.findViewById(R.id.supportButton);
 
 
 
-
-
-
+        welcomeText = findViewById(R.id.welcomeText);
+        currentBalance = findViewById(R.id.currentBalance);
+        addIncomeButton = findViewById(R.id.addIncomeButton);
+        addExpenseButton = findViewById(R.id.addExpenseButton);
+        historyButton = findViewById(R.id.historyButton);
+        profileButton = findViewById(R.id.profileButton);
+        categoryManagementButton = findViewById(R.id.categoryManagementButton);
+        supportButton = findViewById(R.id.supportButton);
     }
 }
