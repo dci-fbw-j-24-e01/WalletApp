a0d70e8d2f5946a457fff2f6c3010230f59f74c4
Robin <robin.hagolani@dci-student.org>
2024-07-04 15:49:16 +0200

    Add calendar functionality

--- app/src/main/java/org/dci/walletapp/IncomeActivity.java
+++ app/src/main/java/org/dci/walletapp/IncomeActivity.java
@@ -1,5 +1,6 @@
 package org.dci.walletapp;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +15,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Calendar;
+
 public class IncomeActivity extends AppCompatActivity {
 
     private TextView titleTextView;
@@ -25,6 +28,8 @@ public class IncomeActivity extends AppCompatActivity {
     private Button saveButton;
     private Button cancelButton;
     private Button goBackButton;
+
+    private Calendar calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +50,7 @@ public class IncomeActivity extends AppCompatActivity {
         saveButton = findViewById(R.id.saveButton);
         cancelButton = findViewById(R.id.cancelButton);
         goBackButton = findViewById(R.id.backButton);
+        calendar = Calendar.getInstance();
 
         goBackButton.setOnClickListener(view -> {
             updateButtonVisibility(true);
@@ -59,7 +65,33 @@ public class IncomeActivity extends AppCompatActivity {
 
         });
 
+        dateEditText.setOnClickListener(view -> showDatePicker() );
+
+
+
     }
+
+    private void showDatePicker() {
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                (view, selectedYear, selectedMonth, selectedDay) -> {
+                    calendar.set(Calendar.YEAR, selectedYear);
+                    calendar.set(Calendar.MONTH, selectedMonth);
+                    calendar.set(Calendar.DAY_OF_MONTH, selectedDay);
+                    updateDateEditText();
+                }, year, month, day);
+        datePickerDialog.show();
+    }
+
+    private void updateDateEditText() {
+        String format = "MM/dd/yyyy"; // You can use any format you prefer
+        java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat(format, java.util.Locale.US);
+        dateEditText.setText(sdf.format(calendar.getTime()));
+    }
+
     private void updateButtonVisibility(boolean isProfileEditable) {
         int goBackVisibility = isProfileEditable ? View.INVISIBLE : View.VISIBLE;
         int editModeVisibility = isProfileEditable ? View.VISIBLE : View.INVISIBLE;
