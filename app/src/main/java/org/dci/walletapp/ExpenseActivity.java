f33830a4d93c8a6b4e75960b716ea7681a3e7253
Herr Oleh Bryl <olegbryl@gmail.com>
2024-07-19 09:38:41 +0200

    added the mask. It works. But for some reason the data is not written to JSON

--- app/src/main/java/org/dci/walletapp/ExpenseActivity.java
+++ app/src/main/java/org/dci/walletapp/ExpenseActivity.java
@@ -86,6 +86,8 @@ public class ExpenseActivity extends AppCompatActivity {
 //        dateEditText.setOnClickListener(view -> showDatePicker());
         dateEditTextImg.setOnClickListener(view -> showDatePicker());
         dateEditText.setText(currentDate);
+        dateEditText.addTextChangedListener(textWatcher);
+
 //        timeEditText.setOnClickListener(view -> showTimePicker());
         timeEditTextImg.setOnClickListener(view -> showTimePicker());
         timeEditText.setText(currentTime);
@@ -115,7 +117,59 @@ public class ExpenseActivity extends AppCompatActivity {
             }
         });
     }
+    TextWatcher textWatcher = new TextWatcher() {
+        private String current = "";
+        private String ddmmyyyy = "DDMMYYYY";
+        private Calendar cal = Calendar.getInstance();
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if (!s.toString().equals(current)) {
+                String clean = s.toString().replaceAll("[^\\d.]|\\.", "");
+                String cleanC = current.replaceAll("[^\\d.]|\\.", "");
 
+                int cl = clean.length();
+                int sel = cl;
+                for (int i = 2; i <= cl && i < 6; i += 2) {
+                    sel++;
+                }
+
+                if (clean.equals(cleanC)) sel--;
+
+                if (clean.length() < 8){
+                    clean = clean + ddmmyyyy.substring(clean.length());
+                }else{
+
+                    int day  = Integer.parseInt(clean.substring(0,2));
+                    int mon  = Integer.parseInt(clean.substring(2,4));
+                    int year = Integer.parseInt(clean.substring(4,8));
+
+                    mon = mon < 1 ? 1 : mon > 12 ? 12 : mon;
+                    cal.set(Calendar.MONTH, mon-1);
+                    year = (year<1900)?1900:(year>2100)?2100:year;
+                    cal.set(Calendar.YEAR, year);
+
+
+                    day = (day > cal.getActualMaximum(Calendar.DATE))? cal.getActualMaximum(Calendar.DATE):day;
+                    clean = String.format("%02d%02d%02d",day, mon, year);
+                }
+
+                clean = String.format("%s/%s/%s", clean.substring(0, 2),
+                        clean.substring(2, 4),
+                        clean.substring(4, 8));
+
+                sel = sel < 0 ? 0 : sel;
+                current = clean;
+                dateEditText.setText(current);
+                dateEditText.setSelection(sel < current.length() ? sel : current.length());
+            }
+        }
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+        @Override
+        public void afterTextChanged(Editable s) {}
+
+    };
     private void setupFieldsIds() {
         dateEditTextImg = findViewById(R.id.dateEditTextImg);
         timeEditTextImg = findViewById(R.id.timeEditTextImg);
@@ -334,6 +388,7 @@ public class ExpenseActivity extends AppCompatActivity {
 //        String dateFormat ="dd/MM/yyyy";
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormat, Locale.ENGLISH);
         dateEditText.setText(simpleDateFormat.format(calendar.getTime()));
+
 
     }
 
